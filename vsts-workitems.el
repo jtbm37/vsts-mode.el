3d57a488aa9e6040b7cdff0b97db38379d32e79f
jtbm37 <jtbm37@users.noreply.github.com>
2017-10-28 21:53:09 +0100

    Added related work items to wi info panel

--- vsts-workitems.el
+++ vsts-workitems.el
@@ -42,13 +42,18 @@
 			       "Microsoft.VSTS.TCM.SystemInfo"
 			       "Microsoft.VSTS.Common.AcceptanceCriteria"))
 
-(defun vsts/get-work-items (ids &optional fields)
-  "Returns work items for the specified ids"
-  (when ids
+(defun vsts/get-work-items (ids &optional fields relation-p)
+  "Returns work items for the specified ids
+
+`fields' is a list of fields. Cannot be used when `relation-p' is non-nil.
+
+When `relation-p' is non-nil, it will include the work item relations."
+  (when (and ids (> (length (remove-if-not 'nil ids)) 0))
     (let ((url (concat (vsts/get-url vsts-workitems-api)
 		       "&ids="
 		       (string-join ids ",")
-		       (when fields (concat "&fields=" (string-join fields ","))))))
+		       (when fields (concat "&fields=" (string-join fields ",")))
+		       (when relation-p "&$expand=relations"))))
       (alist-get 'value (request-response-data (vsts--submit-request url nil "GET" nil nil))))))
 
 (bui-define-interface vsts-wi info
@@ -58,16 +63,25 @@
 	    (Microsoft\.VSTS\.Common\.Priority . "Priority")
 	    (Microsoft\.VSTS\.Common\.AcceptanceCriteria . "Acceptance Criteria"))
   :get-entries-function '(lambda (&rest args)
-			   (let* ((resp (vsts/get-work-items (alist-get 'id args) vsts-workitem-fields))
-				  (value (elt resp 0))
-				  (fields (alist-get 'fields value)))
-			     (list (push (assoc 'id value) fields))))
+			   (list (vsts/get-work-item-info (alist-get 'id args))))
   :format '(vsts-wi-info-title-insert
 	    (System.State format (format))
 	    (Microsoft\.VSTS\.Common\.Priority format (format))
 	    nil
 	    (System.Description vsts-insert-title vsts-insert-html)
-	    (Microsoft.VSTS.Common.AcceptanceCriteria vsts-insert-title vsts-insert-html)))
+	    (Microsoft.VSTS.Common.AcceptanceCriteria vsts-insert-title vsts-insert-html)
+	    vsts-wi-info-relations-insert))
+
+(defun vsts/get-work-item-info (id)
+  "Returns work details necessary for the info panel"
+  (when-let ((resp (vsts/get-work-items id nil t))
+	     (value (elt resp 0))
+	     (fields (alist-get 'fields value))
+	     (id (assoc 'id value))
+	     (relations (assoc 'relations value)))
+    (push id fields)
+    (push relations fields)
+    fields))
 
 (defun vsts-wi-info-title-insert (entry)
   "Inserts the work item title into info buffer"
@@ -79,6 +93,34 @@
     (bui-format-insert "Assigned To" 'bui-info-param-title bui-info-param-title-format)
     (bui-format-insert (replace-regexp-in-string " <.*?>" "" user) )
     (bui-newline)))
+
+(defun vsts-wi-info-relations-insert (entry)
+  "Inserts the related items into info buffer"
+  (when-let ((wis (vsts/get-related-work-items entry)))
+    (bui-format-insert "Related Work Items" 'bui-info-param-title nil)
+    (bui-newline)
+    (seq-doseq (rel wis)
+      (bui-format-insert (format "%s - %s (%s)" (alist-get 'id rel) (alist-get 'System\.Title rel) (alist-get 'System\.State rel)))
+      (bui-newline))))
+
+(defun vsts/get-related-work-items (wi)
+  "Returns a list of the related work items
+in work item `wi'"
+  (when-let ((relations (alist-get 'relations wi))
+	     (rel-wis (remove-if-not '(lambda (x) (string-equal (alist-get 'rel x) "System.LinkTypes.Hierarchy-Forward"))
+				     (alist-get 'relations vsts-temp-wi)))
+	     (ids (mapcar 'vsts/get-relation-work-item-id rel-wis))
+	     (wis (vsts/get-work-items ids '("System.Title" "System.State"))))
+    (mapcar '(lambda (x)
+	       (push (assoc 'id x) (alist-get 'fields x)))
+	    wis)))
+
+(defun vsts/get-relation-work-item-id (relation)
+  "Returns the id of the relation's work item"
+  (let* ((url (alist-get 'url relation))
+	 (id (-last-item (s-split "/" url))))
+    (when (string-to-number id)
+      id)))
 
 (defun vsts/show-workitem (id)
   "Display the work item details"
