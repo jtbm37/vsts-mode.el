a2b9b7fbf56ca37da6521a223bd019ec58b3a808
matrix <matrix@matrix-lnx.lan>
2018-12-26 19:48:42 +0000

    Add newline in comment section to make it clearer

--- vsts-org.el
+++ vsts-org.el
@@ -202,6 +202,7 @@ with all `wi' details"
 								(name (alist-get 'displayName author))
 								(date (alist-get 'revisedDate cmt)))
 						      (insert (format "*** %s on %s: " name date))
+						      (insert hard-newline)
 						      (vsts-insert-html text nil)))))
 				(org-entry-put (point) "type" (alist-get 'System.WorkItemType wi))
 				(org-entry-put (point) "id" (int-to-string (alist-get 'id wi)))
